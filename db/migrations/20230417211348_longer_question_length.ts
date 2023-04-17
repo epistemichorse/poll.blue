ac06e3cb3805d976c5ade5108ac0f868aa09b9aa
Epistemic Horse <130928909+epistemichorse@users.noreply.github.com>
2023-04-17 21:18:16 +0200

    Longer max length for questions

---
+++ db/migrations/20230417211348_longer_question_length.ts
@@ -0,0 +1,15 @@
+import { AbstractMigration, Info, ClientPostgreSQL } from "https://deno.land/x/nessie@2.0.10/mod.ts";
+
+export default class extends AbstractMigration<ClientPostgreSQL> {
+    /** Runs on migrate */
+    async up(info: Info): Promise<void> {
+        this.client.queryObject`
+        ALTER TABLE polls
+            ALTER COLUMN question TYPE character varying(200);
+        `
+    }
+
+    /** Runs on rollback */
+    async down(info: Info): Promise<void> {
+    }
+}
