946d6cd0596c60037549cb10d387a8f9fec3a417
Epistemic Horse <130928909+epistemichorse@users.noreply.github.com>
2023-04-17 18:55:19 +0200

    Better testing

--- bot/bot_test.ts
+++ bot/bot_test.ts
@@ -1,9 +1,20 @@
 import { assertEquals } from "https://deno.land/std@0.183.0/testing/asserts.ts";
 import { Bot } from "./bot.ts";
-import { default as Agent, AppBskyNotificationListNotifications } from "https://esm.sh/v115/@atproto/api@0.2.3"
+import {
+    default as Agent,
+    AppBskyNotificationListNotifications,
+    AppBskyFeedPost,
+    ComAtprotoRepoCreateRecord
+} from "https://esm.sh/v115/@atproto/api@0.2.3";
 import notificationsFixture from './fixtures/notifications.json' assert { type: "json" };
 import questionsFixture from './fixtures/questions.json' assert { type: "json" };
 import { Client } from "https://deno.land/x/postgres@v0.17.0/mod.ts";
+import {
+    assertSpyCall,
+    spy,
+    Spy
+} from "https://deno.land/std@0.177.0/testing/mock.ts";
+import TTL from "https://deno.land/x/ttl/mod.ts";
 
 function fakeAgent(): Agent {
     return {
@@ -15,6 +26,18 @@ function fakeAgent(): Agent {
                         listNotifications: (_params?: AppBskyNotificationListNotifications.QueryParams | undefined, _opts?: AppBskyNotificationListNotifications.CallOptions | undefined): Promise<AppBskyNotificationListNotifications.Response> => {
                             return Promise.resolve(notificationsFixture);
                         }
+                    },
+                    feed: {
+                        post: {
+                            create: spy((_params: Omit<ComAtprotoRepoCreateRecord.InputSchema, 'collection' | 'record'>, _record: AppBskyFeedPost.Record, _headers?: Record<string, string>) => {
+                                return;
+                            })
+                        },
+                        like: {
+                            create: spy(() => {
+                                return;
+                            })
+                        }
                     }
                 }
             }
@@ -22,11 +45,39 @@ function fakeAgent(): Agent {
     } as unknown as Agent
 }
 
-function setup(): Bot {
-    const bot = new Bot({ username: "bottestaccount.bsky.social", password: "password", dbClient: {} as Client });
+function fakeDbClient(): Client {
+    return {
+        queryObject: () => {
+            return {
+                rows: []
+            }
+        }
+    } as unknown as Client
+}
+
+async function setup(): Promise<Bot> {
+    const bot = new Bot({
+        username: "bottestaccount.bsky.social",
+        password: "password",
+        dbClient: fakeDbClient(),
+        postUriCache: new Map() as unknown as TTL<boolean>
+    });
     const agent = fakeAgent();
-    bot.setupAgent(agent);
+    await bot.setupAgent(agent);
     return bot;
+}
+
+function makeReplyRef(): AppBskyFeedPost.ReplyRef {
+    return {
+        parent: {
+            cid: 'a',
+            uri: 'b',
+        },
+        root: {
+            cid: 'c',
+            uri: 'd',
+        }
+    }
 }
 
 Deno.test("filters notifications", async () => {
@@ -35,8 +86,8 @@ Deno.test("filters notifications", async () => {
     assertEquals(notifs.length, 1)
 })
 
-Deno.test("parses notifications", () => {
-    const bot = setup();
+Deno.test("parses notifications", async () => {
+    const bot = await setup();
     for (const question of questionsFixture) {
         const parsed = bot.parseNotification(question.text);
         assertEquals(parsed.question, question.question);
@@ -44,4 +95,36 @@ Deno.test("parses notifications", () => {
             assertEquals(parsed.answers[index], answer);
         }
     }
+})
+
+Deno.test("posts polls", async () => {
+    const bot = await setup();
+    const question = {
+        question: "Question",
+        answers: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"]
+    }
+    const { visibleId: id, createdAt } = (await bot.postPoll({
+        question: question.question,
+        answers: question.answers,
+        enumeration: 'lower',
+    }, makeReplyRef(), 'epistemic.horse'))!;
+    assertSpyCall(bot.Agent?.api.app.bsky.feed.post.create! as unknown as Spy, 0,
+        {
+            args: [
+                { repo: undefined },
+                {
+                    text: "Vote a\nVote b\nVote c\nVote d\n\nShow results",
+                    reply: { parent: { cid: "a", uri: "b" }, root: { cid: "c", uri: "d" } },
+                    facets: [
+                        { index: { byteStart: 0, byteEnd: 6 }, features: [{ $type: "app.bsky.richtext.facet#link", uri: `https://poll.blue/p/${id}/1` }] },
+                        { index: { byteStart: 7, byteEnd: 13 }, features: [{ $type: "app.bsky.richtext.facet#link", uri: `https://poll.blue/p/${id}/2` }] },
+                        { index: { byteStart: 14, byteEnd: 20 }, features: [{ $type: "app.bsky.richtext.facet#link", uri: `https://poll.blue/p/${id}/3` }] },
+                        { index: { byteStart: 21, byteEnd: 27 }, features: [{ $type: "app.bsky.richtext.facet#link", uri: `https://poll.blue/p/${id}/4` }] },
+                        { index: { byteStart: 28, byteEnd: 41 }, features: [{ $type: "app.bsky.richtext.facet#link", uri: `https://poll.blue/p/${id}/0` }] },
+                    ],
+                    createdAt,
+                }
+            ]
+        });
+    assertSpyCall(bot.Agent?.api.app.bsky.feed.like.create! as unknown as Spy, 0, {});
 })
